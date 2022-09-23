dbca781c6aec31931fe156072fa0f3407c112320
yalem123 <yalembrhankelayneh@gmail.com>
2022-09-22 17:30:01 -0700

    submit

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * 2-binary_tree_insert_right - create a new node at the right child
+ * @parent: pointer to parent node
+ * @value: key
+ * Return: Pointer of the new node
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+binary_tree_t *new;
+if (!parent)
+	return (NULL);
+new = binary_tree_node(parent, value);
+if (!new)
+return (NULL);
+if (parent->right != NULL)
+{
+new->right = parent->right;
+new->right->parent = new;
+}
+parent->right = new;
+return (new);
+}
